f9bc0693a41fa4e40dae7220b4176455f1072130
Rob Parrett <robparrett@gmail.com>
2024-05-23 07:47:18 -0700

    More example

--- bevy_simple_prefs/examples/blah.rs
+++ bevy_simple_prefs/examples/blah.rs
@@ -21,6 +21,8 @@ fn main() {
     app.add_plugins(DefaultPlugins);
     app.add_plugins(PreferencesPlugin::<ExampleStruct>::default());
     app.add_systems(Update, changed);
+    app.add_systems(Startup, setup);
+    app.add_systems(Update, button_system);
     app.run();
 }
 
@@ -28,4 +30,71 @@ fn changed(a: Res<A>) {
     if a.is_changed() {
         info!("a is changed! {}", a.val);
     }
+}
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiImage),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut a: ResMut<A>,
+) {
+    for (interaction, mut image) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                image.color = PRESSED_BUTTON;
+                a.val += 1;
+            }
+            Interaction::Hovered => {
+                image.color = HOVERED_BUTTON;
+            }
+            Interaction::None => {
+                image.color = NORMAL_BUTTON;
+            }
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    // ui camera
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    image: UiImage::default().with_color(NORMAL_BUTTON),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Button",
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::srgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        });
 }
