978b6c56a3d063c819d45c6990aee7ab20b7f4b8
Rob Parrett <robparrett@gmail.com>
2025-05-23 21:40:20 -0700

    Add separate setting for `localStorage` key (#12)
    Co-authored-by: Ben Frankel <ben.frankel7@gmail.com>

--- bevy_simple_prefs/examples/home_dir.rs
+++ bevy_simple_prefs/examples/home_dir.rs
@@ -3,6 +3,9 @@
 use bevy::{log::LogPlugin, prelude::*};
 use bevy_simple_prefs::{Prefs, PrefsPlugin};
 
+#[cfg(not(target_arch = "wasm32"))]
+use anyhow::Context;
+
 #[derive(Resource, Reflect, Default, Clone)]
 struct Launches(u32);
 
@@ -11,26 +14,35 @@ struct ExamplePrefs {
     launches: Launches,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     App::new()
         .add_plugins((
             DefaultPlugins.set(LogPlugin {
-                filter: "home_dir=debug,bevy_simple_prefs=debug".into(),
+                filter: "dirs=debug,bevy_simple_prefs=debug".into(),
                 ..default()
             }),
             PrefsPlugin::<ExamplePrefs> {
-                // This value won't be used in WASM builds
-                path: home::home_dir().unwrap_or_default(),
-                // Setting this is optional. `(your_package_name)_prefs.ron` will be used by default.
-                filename: "custom_filename.ron".to_string(),
+                // This example uses the `dirs` crate to locate the user's "local config
+                // directory", e.g. `C:\Users\Alice\AppData\Local` on Windows, or
+                // `/usr/home/alice/.config` on Linux.
+                #[cfg(not(target_arch = "wasm32"))]
+                path: {
+                    let dir = dirs::config_local_dir()
+                        .context("Determining local config directory")?
+                        .join("home_dir");
+                    std::fs::create_dir_all(&dir).context("Creating prefs directory")?;
+                    dir.join("prefs.ron")
+                },
                 ..default()
             },
         ))
         .add_systems(Update, print)
         .run();
+
+    Ok(())
 }
 
-fn print(launches: ResMut<Launches>) {
+fn print(launches: Res<Launches>) {
     if launches.is_changed() && !launches.is_added() {
         info!("Launches: {}", launches.0);
     }
